60f6ce5983c0cf362c957b9c9b6d452678ca5ba6
bodhijb <jf2mc3@gmail.com>
2021-02-19 22:06:44 +0000

    Initial commit

---
+++ app/src/main/java/com/jf2mc1/a015004tgitandroidstudio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jf2mc1.a015004tgitandroidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
